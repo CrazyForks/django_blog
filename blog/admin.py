9b0dafa01888f7bb8c615791b4ebaa1f49cb28fe
ghost <>
2021-10-11 15:51:22 +0800

    完善admin

--- blog/admin.py
+++ blog/admin.py
@@ -6,12 +6,15 @@ from .models import Category,Article
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sort', 'name', )
+    list_display = ('id', 'sort', 'name',  'created',  'modified', )
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'title', )
+    list_display = ('id', 'title',  'category', 'status', 'is_original', 'click',  'created',  'modified', )
+    list_filter = ('status', 'is_original', 'category',)
+    search_fields = ('title', 'content')
+
 
 
 
