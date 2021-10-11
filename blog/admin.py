5d66883ba3e6f1aa7de7fcbc4a99ac294422f9dd
ghost <>
2021-10-11 19:30:39 +0800

    admin 支持 版本管理

--- blog/admin.py
+++ blog/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
+from reversion.admin import VersionAdmin
 
 # Register your models here.
 
 from .models import Category,Article
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(VersionAdmin):
     list_display = ('id','name',  'sort',  'created',  'modified', )
     list_display_links = ('id', 'name',)
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(VersionAdmin):
     list_display = ('id', 'title',  'category', 'is_public', 'is_original', 'click',  'created',  'modified', )
     list_filter = ('is_public', 'is_original', 'category',)
     list_display_links = ('id', 'title',)
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'content_markdown')
 
 
 
