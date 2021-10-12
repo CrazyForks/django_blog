f8b6b9b9fe5f44c868a10548bdd3430215cd3c72
ghost <>
2021-10-12 18:03:37 +0800

    搜索 ok

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView,DetailView,MonthArchiveView
 from django.views.generic.dates import MonthArchiveView ,YearArchiveView
 from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
-
+from django.db.models import Q
 from django.conf import settings
 
 from .models import Article,Category
@@ -15,19 +15,26 @@ class ArticleListView(ListView):
     # queryset = Article.get_public_article()
     paginate_by = 10
 
+
     def get_queryset(self):
         pk = self.kwargs.get("pk", None)
+        q = self.request.GET.get('q', None)  # 搜索关键词
         if pk:
             articles = Article.get_public_article().filter(category=pk)
         else:
             articles = Article.get_public_article()
+
+        if q:
+            articles = articles.filter(Q(title__contains=q) | Q(content_markdown__contains=q))
+
         return articles
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['SITE_CONFIG'] = settings.SITE_CONFIG  # 网站配置信息
         context['category'] = Category.objects.all()  # 网站分类
-        print(1)
+        context['q'] = self.request.GET.get('q', None)  # 搜索关键词
+
         return context
 
 
